30dc6cb3e385df1ab08f7f15c896b70225b10b31
AfikPeretz <afikperetz5235@gmail.com>
2022-09-14 13:17:11 +0300

    leetcode

---
+++ LeetCode-Practicing/RemoveKDigits/Solutions/Solutions.py
@@ -0,0 +1,14 @@
+# O(n) complexity, If you didn't pass the question, don't be upset, even though the question is listed at the "medium" level,
+# it is more difficult than many questions at the "hard" level that I have solved
+def removeKdigits(num, k):
+    if len(num) <= k:
+        return "0"
+    stack = []
+    for c in num:
+        while k>0 and stack and stack[-1]>c:
+            stack.pop()
+            k -=1
+        stack.append(c)
+    stack = stack [: len(stack) - k]
+    res = "".join(stack)
+    return str(int(res))
