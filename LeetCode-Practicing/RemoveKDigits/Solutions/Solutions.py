c3f11e311d6dcee54afbd6849dcbf12c7b2ab39f
Afik Peretz <73401652+AfikPeretz@users.noreply.github.com>
2022-09-14 13:36:42 +0300

    Update Solutions.py

--- LeetCode-Practicing/RemoveKDigits/Solutions/Solutions.py
+++ LeetCode-Practicing/RemoveKDigits/Solutions/Solutions.py
@@ -1,5 +1,5 @@
-# O(n) complexity, If you didn't pass the question, don't be upset, even though the question is listed at the "medium" level,
-# it is more difficult than many questions at the "hard" level that I have solved
+# O(n) complexity, If you didn't pass the question don't be upset, even though the question is listed at the "medium" level,
+# it is more difficult than many questions at the "hard" level that I have solved.
 def removeKdigits(num, k):
     if len(num) <= k:
         return "0"
